70c99ea64a33b9eccbdc4d68dc0346a24e46bd54
TuDUs98 <tutixmacho@gmail.com>
2020-03-20 11:00:43 +0400

    добавил в гит бдмелкие исправления, прям чу-чуть поработал с кодом css

--- data/create_db.py
+++ data/create_db.py
@@ -2,6 +2,5 @@ from data import db_session
 from data.db_functions import *
 
 db_session.global_init("db/db.sqlite")
-add_user('admin', 'qwerty@mail.ru', '1234')
-add_user('admin2', 'qwerty2@mail.ru', '12342')
-add_user('admin3', 'qwerty3@mail.ru', '12343')
+
+add_user('admin', 'kaktak.group@gmail.ru', '40221987')
